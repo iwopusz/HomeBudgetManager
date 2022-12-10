5b1e8561d0ebb49046cb18ee78735db6492d940d
Łukasz Pałka <palkalukasz1@gmail.com>
2022-12-10 10:12:02 +0100

    pagination update (css params, jsp view)

--- src/main/java/pl/home/paymentsmanagement/controller/HistoryController.java
+++ src/main/java/pl/home/paymentsmanagement/controller/HistoryController.java
@@ -6,8 +6,14 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import pl.home.paymentsmanagement.model.Charger;
+import pl.home.paymentsmanagement.model.Household;
+import pl.home.paymentsmanagement.model.Person;
 import pl.home.paymentsmanagement.model.Transaction;
+import pl.home.paymentsmanagement.service.ChargerService;
 import pl.home.paymentsmanagement.service.HistoryService;
+import pl.home.paymentsmanagement.service.HouseholdService;
+import pl.home.paymentsmanagement.service.PersonService;
 
 import java.util.List;
 
@@ -15,9 +21,15 @@ import java.util.List;
 public class HistoryController {
 
     private final HistoryService historyService;
+    private final HouseholdService householdService;
+    private final PersonService personService;
+    private final ChargerService chargerService;
 
-    public HistoryController(HistoryService historyService) {
+    public HistoryController(HistoryService historyService, HouseholdService householdService, PersonService personService, ChargerService chargerService) {
         this.historyService = historyService;
+        this.householdService = householdService;
+        this.personService = personService;
+        this.chargerService = chargerService;
     }
 
     @ModelAttribute("historyPage")
@@ -30,8 +42,24 @@ public class HistoryController {
         return getHistory(page).getContent();
     }
 
+    @ModelAttribute("households")
+    public List<Household> getHouseholdList() {
+        return householdService.getHouseholdList();
+    }
+
+    @ModelAttribute("people")
+    public List<Person> getPeople() {
+        return personService.getPersons();
+    }
+
+    @ModelAttribute("chargers")
+    public List<Charger> getChargers() {
+        return chargerService.getChargers();
+    }
+
     @GetMapping(value = {"/history", "/history/"})
     public String getHistory() {
-        return "/management/history";
+        return "management/history";
     }
+
 }
